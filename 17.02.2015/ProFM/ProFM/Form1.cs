9cfb71b2ca4edd28265ec4b70503d6cdf9fe8da8
ProGrupaBitola <kristina@progrupa.mk>
2015-02-20 13:30:26 +0100

    test2

--- 17.02.2015/ProFM/ProFM/Form1.cs
+++ 17.02.2015/ProFM/ProFM/Form1.cs
@@ -1402,7 +1402,6 @@ namespace ProFM
         private void btnNajava_Click(object sender, EventArgs e)
         {
             //komentar
-            //komentar2
             //komentar3
             bool boolAvtentificiran = false;
             string uloga = "";
